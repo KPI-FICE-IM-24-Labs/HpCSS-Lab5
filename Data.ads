f046905f945f21a40983fe884217413cffa7da5f
Kiril Yakymchuk <stbestich@gmail.com>
2025-05-17 16:26:16 +0300

    interface for Data

---
+++ Data.ads
@@ -0,0 +1,19 @@
+package Data is
+    N : constant := 6; -- розмір векторів та матриць
+    H : constant := 2; -- розмір підматриці
+
+    type VectorN is array (1 .. N) of Integer;
+    type VectorH is array (1 .. N) of Integer;
+
+    type Matrix is array (1 .. N) of VectorN;
+    type MatrixH is array (1 .. N) of VectorH;
+
+    function fillVector return VectorN;
+    function fillMatrix return Matrix;
+    function fillSubMatrix (Thread : Integer; M : Matrix) return MatrixH;
+
+    function multiplyVectorByMatrix (vector : VectorN; Matrix : MatrixH) return VectorH;
+    function multiplyMatrixByMatrix (matrixA, matrixB : Matrix) return MatrixH;
+    function addTwoVectors (vectorA, vectorB: VectorH) return VectorH;
+    function findMinumumFromVector (vector: VectorH) return Integer;
+end Data;
